2eba591db28dd10459e8fb759771203a66db971b
Barkó Zsolt <barko.zsolte@gmail.com>
2021-12-13 22:01:04 +0100

    feat: create basic design; add display and template handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CSSGridDemo';
+  displayFormValue = 'grid';
+  gridTemplateColumns = '1fr 1fr';
+  gridTemplateRows = '1fr 1fr';
+  cubeArray: number[] = [1, 2];
+
+  addCube(): void {
+    const lastItem: number = this.cubeArray.pop();
+    this.cubeArray.push(lastItem);
+    this.cubeArray.push(lastItem + 1);
+  }
 }
